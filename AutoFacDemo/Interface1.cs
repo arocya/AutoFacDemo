c377e383255295cff18226cc3967d4ed545bd843
Arion Almond <aalmond@avidxchange.com>
2018-05-13 16:42:42 -0400

    adding to github

---
+++ AutoFacDemo/Interface1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AutoFacDemo
+{
+    public interface IOutput
+    {
+        void Write(string content);
+    }
+
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+
+    public interface IDateWriter
+    {
+        void WriteDate();
+        
+    }
+
+    public class TodayWriter : IDateWriter
+    {
+        private IOutput _output;
+
+        public TodayWriter(IOutput output)
+        {
+            this._output = output;
+        }
+
+        public void WriteDate()
+        {
+            this._output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+
+}
--- AutoFacDemo/Program.cs
+++ AutoFacDemo/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac;
 
 namespace AutoFacDemo
 {
     class Program
     {
+        // http://autofac.readthedocs.io/en/latest/getting-started/index.html
+
+        private static IContainer Container { get; set; }
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            WriteDate();
+        }
+
+        public static void WriteDate()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+                
+            }
         }
     }
 }
